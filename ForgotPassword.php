6bb6d50869d42c55c6da03fc634cd433ad88a85d
John Damasene Ochillo <35804245+JDamascene@users.noreply.github.com>
2018-03-21 11:14:55 +0300

    Add files via upload

--- ForgotPassword.php
+++ ForgotPassword.php
@@ -1,51 +1,44 @@
 <?php 
 	session_start();
 	include("Includes/Header.inc.php");
+
 ?>
+
 <div class="container text-center">
+  <h1 style="color: #fff;">Forgot Your Password</h1>
+  <h4 style="color: #fff;">Plese Fill In The Form</h4>
 	<form action="ForgotPassword.php" method="post">
-		<input type="text" name="Name" required><br>
-		<input type="text" name="phonenumber" required><br>
-		<button type="submit" name="submit">Check Account</button>
+		<input type="text" name="Name" style='width: 100%; height:40px; border-radius: 10px;' placeholder="User Name" required>
+    <br><br>
+		<input type="text" name="phonenumber" style='width: 100%; height:40px; border-radius: 10px;'  placeholder="Phone Number" required>
+    <br><br>
+		<button type="submit" name="CheckResults">Check Account</button>
 	</form>
 </div>
 
+
 <?php 
-	if (isset($_POST['submit'])) {
-		extract($_POST);
-		require "Includes/DBh.inc.php";
-      $Name = mysqli_real_escape_string($Conn,$_POST['Name']);
-      $phonenumber = mysqli_real_escape_string($Conn,$_POST['pasword']);
+  if (isset($_POST['CheckResults'])) {
+  extract($_POST); 
 
-      $sql = "SELECT * FROM 6470users WHERE USERNAME = '$Name' , PHONE = '$phonenumber' `PASWORD` = '$hashedPssword'";
-  		$Result = mysqli_query($Conn,$sql);
-  		$resultCheck = mysqli_num_rows($Result);
+  $Name = mysqli_real_escape_string($Conn,$_POST['Name']);
+  $phonenumber = mysqli_real_escape_string($Conn,$_POST['phonenumber']);
 
-      if ($resultCheck < 1) {
-        header('Location: ForgotPassword.php?LogIn=No Match Found');
-        exit();
-      } else {
-        #Verifying The Password
-        if ($Row = mysqli_fetch_assoc($Result)) {
-          $_SESSION['User'] = $Name;
-          $_SESSION['tel'] = $phonenumber;
-          
-          $hashedPssword = password_verify($pasword ,$Row['PASWORD']);
-          $p = $hashedPssword;
-          echo "You Password Is $p";
-        } 
-      }
-    } else {
-      header('Location: index.php');
-      exit();
-    }
-  		
+  $sql = "SELECT * FROM 6470users WHERE USERNAME = '$Name'";
+  $Records = mysqli_query($Conn, $sql);
   
-?>
-
-
-
-	}
+  $row = mysqli_fetch_assoc($Records);
+  $resultCheck = mysqli_num_rows($Records);
+  if ($resultCheck < 1) {
+      $error = "You don't have an acount";
+        echo "<h1 class='text-center'>$error</h1>";
+        exit();
+    } else {
+      //echo "$row[ID]";
+      header("Location: ResetPassword.php?UsID=$row[ID] ");
+    }
+    
+  } 
 ?>
 
 
